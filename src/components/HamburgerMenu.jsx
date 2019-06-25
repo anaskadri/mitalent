e2345d3e4d84581c2136e0d4cc91864896de4240
anaskadri <anaskadri0@gmail.com>
2019-06-25 18:36:48 +0100

    3

--- src/components/HamburgerMenu.jsx
+++ src/components/HamburgerMenu.jsx
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import { slide as Menu } from 'react-burger-menu'
 
 class HamburgerMenu extends React.Component {
+
   showSettings (event) {
     event.preventDefault();
   }
 
   render () {
+    var isMenuOpen = function(state) {
+        return state.isOpen;
+      };
     return (
-      <Menu>
+      <Menu onStateChange={ isMenuOpen } >
         <a id="home" className="menu-item" href="/">Home</a>
         <a id="about" className="menu-item" href="/about">About</a>
         <a id="contact" className="menu-item" href="/contact">Contact</a>
